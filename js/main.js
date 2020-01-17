12e5ca60ddbf3e49a3623a8a528d3dcd520efaeb
Jaskaran Singh <j_jaskaransing108373@ats-hr420-dm12.fc.ca>
2020-01-17 14:00:57 -0500

    adding code

--- js/main.js
+++ js/main.js
@@ -9,19 +9,27 @@
   // find the button element on the page by its id with JavaScript
   let theButton = document.querySelector("#myButton"),
       theHeading = document.querySelector('h1'),
-      svgImage = document.querySelector("#svgGraphic");
+      //svgImage = document.querySelector("#svgGraphic"),
+      allSVGs = document.querySelectorAll(".svg");
 
   // this function changes the heading text
   function changeText() {
     theHeading.textContent = "hello there from the console";
   }
 
-  // log the id of the svg to the console when a user hovers on it 
+  // log the id of the svg to the console when a user hovers on it
   function logSVG() {
     console.log(this.id);
   }
 
   theButton.addEventListener("click", changeText);
-  svgImage.addEventListener("mouseover", logSVG);
+  //svgImage.addEventListener("click", logSVG),
+  //svgImage2.addEventListener("click", logSVG);
+  // loop through the collection and apply our event handling to each element
+  allSVGs.forEach(item => item.addEventListener('click', logSVG));
   //document.querySelector('h1').textContent = "hello there from the console";
+
+
+  //for (let i; i<allSVGs.length; i++) {
+  //for (el in allSVGs) {}
 })();
